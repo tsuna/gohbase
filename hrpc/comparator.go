85243d059364dc4315026cf17fbdb6ca0e9970a4
Sam Curley <CurleySamuel@gmail.com>
2015-07-08 11:41:03 -0700

    Alter Filters/Comparators to accept const vs. str
    A FilterList creation now looks like -
    
    f := hrpc.NewFilterList(hrpc.MustPassAll, filter1, filter2)
    
    Change-Id: Iaf7397dfb919054c5bc6e1d5a76f7fb22a68882c

--- hrpc/comparator.go
+++ hrpc/comparator.go
@@ -7,12 +7,25 @@ package hrpc
 
 import (
 	"errors"
-
 	"github.com/golang/protobuf/proto"
 	"github.com/tsuna/gohbase/pb"
 )
 
 const comparatorPath = "org.apache.hadoop.hbase.filter."
+
+// BitComparatorBitwiseOp is TODO
+type BitComparatorBitwiseOp int32
+
+func (o BitComparatorBitwiseOp) isValid() bool {
+	return (o >= 1 && o <= 3)
+}
+
+// Constants are TODO
+const (
+	BitComparatorAND BitComparatorBitwiseOp = 1
+	BitComparatorOR  BitComparatorBitwiseOp = 2
+	BitComparatorXOR BitComparatorBitwiseOp = 3
+)
 
 // Ensure our types implement Comparator correctly.
 var _ Comparator = (*BinaryComparator)(nil)
@@ -144,11 +157,11 @@ func (c *BinaryPrefixComparator) ConstructPBComparator() (*pb.Comparator, error)
 type BitComparator struct {
 	Name       string
 	Comparable *ByteArrayComparable
-	BitwiseOp  string
+	BitwiseOp  BitComparatorBitwiseOp
 }
 
 // NewBitComparator is TODO
-func NewBitComparator(bitwiseOp string, comparable *ByteArrayComparable) *BitComparator {
+func NewBitComparator(bitwiseOp BitComparatorBitwiseOp, comparable *ByteArrayComparable) *BitComparator {
 	return &BitComparator{
 		Name:       comparatorPath + "BitComparator",
 		Comparable: comparable,
@@ -158,12 +171,10 @@ func NewBitComparator(bitwiseOp string, comparable *ByteArrayComparable) *BitCom
 
 // ConstructPBComparator is TODO
 func (c *BitComparator) ConstructPBComparator() (*pb.Comparator, error) {
-	b := pb.BitComparator_BitwiseOp(1)
-	if val, ok := pb.BitComparator_BitwiseOp_value[c.BitwiseOp]; ok {
-		b = pb.BitComparator_BitwiseOp(val)
-	} else {
-		return nil, errors.New("Invalid bitwise operation specified.")
+	if !c.BitwiseOp.isValid() {
+		return nil, errors.New("Invalid bitwise operator specified")
 	}
+	b := pb.BitComparator_BitwiseOp(c.BitwiseOp)
 	internalComparator := &pb.BitComparator{
 		Comparable: c.Comparable.ConstructPB(),
 		BitwiseOp:  &b,
