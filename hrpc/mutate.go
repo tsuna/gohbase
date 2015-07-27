a1afe2a97a3ea288c5665e53e49ab553121ea186
Derek Gonyeo <dgonyeo@csh.rit.edu>
2015-07-27 11:32:24 -0700

    Adds a reflection-based API for putting new values into HBase.
    Adds alternate functions to hrpc/mutate.go for modifying data. Instead
    of taking nested maps of byte slices, structs can be passed in. The
    exposed fields of the struct will be inspected for hbase tags containing
    column families and qualifiers, and these fields are then converted into
    byte slices and entered into the protobuf messages as if they had come
    from the nested maps like before.
    
    An accompanying commit containing calls for deserializing a pb.Result
    into a tagged struct will be written.
    
    The nested maps are still exposed because the way the new function goes
    about serializing the fields of the struct may not be the most effective
    method, depending on the use case.
    
    Change-Id: I9710be0a483d4c8ade5bc65baa199ac2e80b1707
    Signed-off-by: Benoit Sigoure <tsunanet@gmail.com>

--- hrpc/mutate.go
+++ hrpc/mutate.go
@@ -7,10 +7,29 @@ package hrpc
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"unsafe"
+
 	"github.com/golang/protobuf/proto"
 	"github.com/tsuna/gohbase/filter"
 	"github.com/tsuna/gohbase/pb"
 	"golang.org/x/net/context"
+)
+
+var (
+	// ErrNotAStruct is returned by any of the *Ref functions when something
+	// other than a struct is passed in to their data argument
+	ErrNotAStruct = errors.New("data must be a struct")
+
+	// ErrUnsupportedUints is returned when this message is serialized and uints
+	// are unsupported on your platform (this will probably never happen)
+	ErrUnsupportedUints = errors.New("uints are unsupported on your platform")
+
+	// ErrUnsupportedInts is returned when this message is serialized and ints
+	// are unsupported on your platform (this will probably never happen)
+	ErrUnsupportedInts = errors.New("ints are unsupported on your platform")
 )
 
 // Mutate represents a mutation on HBase.
@@ -20,12 +39,16 @@ type Mutate struct {
 	row          *[]byte
 	mutationType pb.MutationProto_MutationType //*int32
 
-	//values is a map of column families to a map of column qualifiers to bytes
+	// values is a map of column families to a map of column qualifiers to bytes
 	values map[string]map[string][]byte
+
+	// data is a struct passed in that has fields tagged to represent HBase
+	// columns
+	data interface{}
 }
 
 // baseMutate will return a Mutate struct without the mutationType filled in.
-func baseMutate(ctx context.Context, table, key string, values map[string]map[string][]byte) *Mutate {
+func baseMutate(ctx context.Context, table, key string, values map[string]map[string][]byte, data interface{}) *Mutate {
 	return &Mutate{
 		base: base{
 			table: []byte(table),
@@ -33,13 +56,26 @@ func baseMutate(ctx context.Context, table, key string, values map[string]map[st
 			ctx:   ctx,
 		},
 		values: values,
+		data:   data,
 	}
 }
 
 // NewPutStr creates a new Mutation request that will put the given values into
 // HBase under the given table and key.
 func NewPutStr(ctx context.Context, table, key string, values map[string]map[string][]byte) (*Mutate, error) {
-	m := baseMutate(ctx, table, key, values)
+	m := baseMutate(ctx, table, key, values, nil)
+	m.mutationType = pb.MutationProto_PUT
+	return m, nil
+}
+
+// NewPutStrRef creates a new Mutation request that will put the given values into
+// HBase under the given table and key. The data argument must be a struct with
+// fields containing the "hbase" tag.
+func NewPutStrRef(ctx context.Context, table, key string, data interface{}) (*Mutate, error) {
+	if !isAStruct(data) {
+		return nil, ErrNotAStruct
+	}
+	m := baseMutate(ctx, table, key, nil, data)
 	m.mutationType = pb.MutationProto_PUT
 	return m, nil
 }
@@ -47,7 +83,18 @@ func NewPutStr(ctx context.Context, table, key string, values map[string]map[str
 // NewDelStr creates a new Mutation request that will delete the given values
 // from HBase under the given table and key.
 func NewDelStr(ctx context.Context, table, key string, values map[string]map[string][]byte) (*Mutate, error) {
-	m := baseMutate(ctx, table, key, values)
+	m := baseMutate(ctx, table, key, values, nil)
+	m.mutationType = pb.MutationProto_DELETE
+	return m, nil
+}
+
+// NewDelStrRef creates a new Mutation request that will delete the given values
+// from HBase under the given table and key.
+func NewDelStrRef(ctx context.Context, table, key string, data interface{}) (*Mutate, error) {
+	if !isAStruct(data) {
+		return nil, ErrNotAStruct
+	}
+	m := baseMutate(ctx, table, key, nil, data)
 	m.mutationType = pb.MutationProto_DELETE
 	return m, nil
 }
@@ -55,7 +102,18 @@ func NewDelStr(ctx context.Context, table, key string, values map[string]map[str
 // NewAppStr creates a new Mutation request that will append the given values
 // to their existing values in HBase under the given table and key.
 func NewAppStr(ctx context.Context, table, key string, values map[string]map[string][]byte) (*Mutate, error) {
-	m := baseMutate(ctx, table, key, values)
+	m := baseMutate(ctx, table, key, values, nil)
+	m.mutationType = pb.MutationProto_APPEND
+	return m, nil
+}
+
+// NewAppStrRef creates a new Mutation request that will append the given values
+// to their existing values in HBase under the given table and key.
+func NewAppStrRef(ctx context.Context, table, key string, data interface{}) (*Mutate, error) {
+	if !isAStruct(data) {
+		return nil, ErrNotAStruct
+	}
+	m := baseMutate(ctx, table, key, nil, data)
 	m.mutationType = pb.MutationProto_APPEND
 	return m, nil
 }
@@ -63,7 +121,18 @@ func NewAppStr(ctx context.Context, table, key string, values map[string]map[str
 // NewIncStr creates a new Mutation request that will increment the given values
 // in HBase under the given table and key.
 func NewIncStr(ctx context.Context, table, key string, values map[string]map[string][]byte) (*Mutate, error) {
-	m := baseMutate(ctx, table, key, values)
+	m := baseMutate(ctx, table, key, values, nil)
+	m.mutationType = pb.MutationProto_INCREMENT
+	return m, nil
+}
+
+// NewIncStrRef creates a new Mutation request that will increment the given values
+// in HBase under the given table and key.
+func NewIncStrRef(ctx context.Context, table, key string, data interface{}) (*Mutate, error) {
+	if !isAStruct(data) {
+		return nil, ErrNotAStruct
+	}
+	m := baseMutate(ctx, table, key, nil, data)
 	m.mutationType = pb.MutationProto_INCREMENT
 	return m, nil
 }
@@ -76,6 +145,15 @@ func (m *Mutate) GetName() string {
 // Serialize converts this mutate object into a protobuf message suitable for
 // sending to an HBase server
 func (m *Mutate) Serialize() ([]byte, error) {
+	if m.data == nil {
+		return m.serialize()
+	}
+	return m.serializeWithReflect()
+}
+
+// serialize is a helper function for Serialize. It is used when there is a
+// map[string]map[string][]byte to be serialized.
+func (m *Mutate) serialize() ([]byte, error) {
 	// We need to convert everything in the values field
 	// to a protobuf ColumnValue
 	bytevalues := make([]*pb.MutationProto_ColumnValue, len(m.values))
@@ -111,6 +189,232 @@ func (m *Mutate) Serialize() ([]byte, error) {
 		},
 	}
 	return proto.Marshal(mutate)
+}
+
+// serializeWithReflect is a helper function for Serialize. It is used when
+// there is a struct with tagged fields to be serialized.
+func (m *Mutate) serializeWithReflect() ([]byte, error) {
+	typeOf := reflect.TypeOf(m.data)
+	valueOf := reflect.Indirect(reflect.ValueOf(m.data))
+
+	columns := make(map[string][]*pb.MutationProto_ColumnValue_QualifierValue)
+
+	for i := 0; i < typeOf.NumField(); i++ {
+		field := typeOf.Field(i)
+		if field.PkgPath != "" {
+			// This is an unexported field of the struct, so we're going to
+			// ignore it
+			continue
+		}
+
+		tagval := field.Tag.Get("hbase")
+		if tagval == "" {
+			// If the tag is empty, we're going to ignore this field
+			continue
+		}
+		cnames := strings.SplitN(tagval, ":", 2)
+		if len(cnames) != 2 {
+			// If the tag doesn't contain a colon, it's set improperly
+			return nil, fmt.Errorf("Invalid column family and column qualifier: \"%s\"", cnames)
+		}
+		cfamily := cnames[0]
+		cqualifier := cnames[1]
+
+		binaryValue, err := valueToBytes(valueOf.Field(i))
+		if err != nil {
+			return nil, err
+		}
+
+		qualVal := &pb.MutationProto_ColumnValue_QualifierValue{
+			Qualifier: []byte(cqualifier),
+			Value:     binaryValue,
+		}
+
+		if m.mutationType == pb.MutationProto_DELETE {
+			tmp := pb.MutationProto_DELETE_MULTIPLE_VERSIONS
+			qualVal.DeleteType = &tmp
+		}
+		columns[cfamily] = append(columns[cfamily], qualVal)
+	}
+
+	pbcolumns := make([]*pb.MutationProto_ColumnValue, 0, len(columns))
+	for k, v := range columns {
+		colval := &pb.MutationProto_ColumnValue{
+			Family:         []byte(k),
+			QualifierValue: v,
+		}
+		pbcolumns = append(pbcolumns, colval)
+
+	}
+	mutate := &pb.MutateRequest{
+		Region: m.regionSpecifier(),
+		Mutation: &pb.MutationProto{
+			Row:         m.key,
+			MutateType:  &m.mutationType,
+			ColumnValue: pbcolumns,
+		},
+	}
+	return proto.Marshal(mutate)
+}
+
+// valueToBytes will convert a given value from the reflect package into its
+// underlying bytes
+func valueToBytes(val reflect.Value) ([]byte, error) {
+	switch val.Kind() {
+	case reflect.Bool:
+		if val.Bool() {
+			return []byte{1}, nil
+		}
+		return []byte{0}, nil
+
+	case reflect.Uint:
+		switch unsafe.Sizeof(unsafe.Pointer(val.UnsafeAddr())) {
+		case 8:
+			var x uint8
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		case 16:
+			var x uint16
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		case 32:
+			var x uint32
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		case 64:
+			var x uint64
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		default:
+			return nil, ErrUnsupportedUints
+		}
+
+	case reflect.Int:
+		switch unsafe.Sizeof(unsafe.Pointer(val.UnsafeAddr())) {
+		case 8:
+			var x uint8
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		case 16:
+			var x uint16
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		case 32:
+			var x uint32
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		case 64:
+			var x uint64
+			return valueToBytes(val.Convert(reflect.TypeOf(x)))
+		default:
+			return nil, ErrUnsupportedInts
+		}
+
+	case reflect.Int8:
+		var x int8
+		x = val.Interface().(int8)
+		memory := (*(*[1]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+	case reflect.Uint8:
+		var x uint8
+		x = val.Interface().(uint8)
+		memory := (*(*[1]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+
+	case reflect.Int16:
+		var x int16
+		x = val.Interface().(int16)
+		memory := (*(*[2]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+	case reflect.Uint16:
+		var x uint16
+		x = val.Interface().(uint16)
+		memory := (*(*[2]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+
+	case reflect.Int32:
+		var x int32
+		x = val.Interface().(int32)
+		memory := (*(*[4]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+	case reflect.Uint32:
+		var x uint32
+		x = val.Interface().(uint32)
+		memory := (*(*[4]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+	case reflect.Float32:
+		var x float32
+		x = val.Interface().(float32)
+		memory := (*(*[4]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+
+	case reflect.Int64:
+		var x int64
+		x = val.Interface().(int64)
+		memory := (*(*[8]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+	case reflect.Uint64:
+		var x uint64
+		x = val.Interface().(uint64)
+		memory := (*(*[8]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+	case reflect.Float64:
+		var x float64
+		x = val.Interface().(float64)
+		memory := (*(*[8]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+	case reflect.Complex64:
+		var x complex64
+		x = val.Interface().(complex64)
+		memory := (*(*[8]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+
+	case reflect.Complex128:
+		var x complex128
+		x = val.Interface().(complex128)
+		memory := (*(*[16]byte)(unsafe.Pointer(&x)))[:]
+		return copyOf(memory), nil
+
+	case reflect.Ptr:
+		return valueToBytes(val.Elem())
+
+	case reflect.Array, reflect.Slice:
+		if val.Len() == 0 {
+			return []byte{}, nil
+		}
+		kind := val.Index(0).Kind()
+		if kind == reflect.Array || kind == reflect.Slice || kind == reflect.String {
+			// We won't be able to deserialize this later into the correct types, since
+			// arrays/slices/strings don't have a defined size.
+			return nil, fmt.Errorf("Slices and arrays of type %s is unsupported", val.Index(0).Type().Name())
+		}
+		var allbytes []byte
+		for i := 0; i < val.Len(); i++ {
+			morebytes, err := valueToBytes(val.Index(i))
+			if err != nil {
+				return nil, err
+			}
+			allbytes = append(allbytes, morebytes...)
+		}
+		return allbytes, nil
+
+	case reflect.String:
+		return []byte(val.String()), nil
+
+		// Unhandled types, left here for easy reference
+		//case reflect.Invalid:
+		//case reflect.Chan:
+		//case reflect.Func:
+		//case reflect.Interface:
+		//case reflect.Struct:
+		//case reflect.Map:
+		//case reflect.Uintptr:
+		//case reflect.UnsafePointer:
+	}
+	return nil, fmt.Errorf("Unsupported type %s, %d", val.Type().Name(), val.Kind())
+}
+
+func copyOf(memory []byte) []byte {
+	memcpy := make([]byte, len(memory))
+	copy(memcpy, memory)
+	return memcpy
+}
+
+func isAStruct(data interface{}) bool {
+	return reflect.TypeOf(data).Kind() == reflect.Struct
 }
 
 // NewResponse creates an empty protobuf message to read the response of this
