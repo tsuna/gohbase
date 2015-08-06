d695d34dd1663353f793260eb77a7163269c6320
Derek Gonyeo <dgonyeo@csh.rit.edu>
2015-08-06 11:29:52 -0700

    Added benchmarks for the two serialization options in hrpc/mutate.go
    Tests the speed of serializing both nested maps and structs.
    
    Change-Id: Iec41448856313e7e79d151d7f0687b39a29ec1af

--- hrpc/hrpc_test.go
+++ hrpc/hrpc_test.go
@@ -7,10 +7,13 @@ package hrpc
 
 import (
 	"bytes"
-	"github.com/tsuna/gohbase/filter"
-	"golang.org/x/net/context"
 	"reflect"
+	"strconv"
 	"testing"
+
+	"github.com/tsuna/gohbase/filter"
+	"github.com/tsuna/gohbase/regioninfo"
+	"golang.org/x/net/context"
 )
 
 func TestNewGet(t *testing.T) {
@@ -108,4 +111,93 @@ func confirmScanAttributes(s *Scan, ctx context.Context, table, start, stop []by
 		return false
 	}
 	return true
+}
+
+func BenchmarkMutateSerializeWithNestedMaps(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		data := map[string]map[string][]byte{
+			"cf": map[string][]byte{
+				"a": []byte{10},
+				"b": []byte{20},
+				"c": []byte{30, 0},
+				"d": []byte{40, 0, 0, 0},
+				"e": []byte{50, 0, 0, 0, 0, 0, 0, 0},
+				"f": []byte{60},
+				"g": []byte{70},
+				"h": []byte{80, 0},
+				"i": []byte{90, 0, 0, 0},
+				"j": []byte{100, 0, 0, 0, 0, 0, 0, 0},
+				"k": []byte{0, 0, 220, 66},
+				"l": []byte{0, 0, 0, 0, 0, 0, 94, 64},
+				"m": []byte{0, 0, 2, 67, 0, 0, 0, 0},
+				"n": []byte{0, 0, 0, 0, 0, 128, 97, 64, 0, 0, 0, 0, 0, 0, 0, 0},
+				"o": []byte{150},
+				"p": []byte{4, 8, 15, 26, 23, 42},
+				"q": []byte{1, 1, 3, 5, 8, 13, 21, 34, 55},
+				"r": []byte("Run #" + strconv.Itoa(i)),
+			},
+		}
+		mutate, err := NewPutStr(context.Background(), "", "", data)
+		if err != nil {
+			b.Errorf("Error creating mutate: %v", err)
+		}
+		mutate.SetRegion(&regioninfo.Info{})
+		mutate.Serialize()
+	}
+}
+
+func BenchmarkMutateSerializeWithReflection(b *testing.B) {
+	b.ReportAllocs()
+
+	type teststr struct {
+		AnInt       int        `hbase:"cf:a"`
+		AnInt8      int8       `hbase:"cf:b"`
+		AnInt16     int16      `hbase:"cf:c"`
+		AnInt32     int32      `hbase:"cf:d"`
+		AnInt64     int64      `hbase:"cf:e"`
+		AnUInt      uint       `hbase:"cf:f"`
+		AnUInt8     uint8      `hbase:"cf:g"`
+		AnUInt16    uint16     `hbase:"cf:h"`
+		AnUInt32    uint32     `hbase:"cf:i"`
+		AnUInt64    uint64     `hbase:"cf:j"`
+		AFloat32    float32    `hbase:"cf:k"`
+		AFloat64    float64    `hbase:"cf:l"`
+		AComplex64  complex64  `hbase:"cf:m"`
+		AComplex128 complex128 `hbase:"cf:n"`
+		APointer    *int       `hbase:"cf:o"`
+		AnArray     [6]uint8   `hbase:"cf:p"`
+		ASlice      []uint8    `hbase:"cf:q"`
+		AString     string     `hbase:"cf:r"`
+	}
+
+	for i := 0; i < b.N; i++ {
+		str := teststr{
+			AnInt:       i,
+			AnInt8:      int8(i),
+			AnInt16:     int16(i),
+			AnInt32:     int32(i),
+			AnInt64:     int64(i),
+			AnUInt:      uint(i),
+			AnUInt8:     uint8(i),
+			AnUInt16:    uint16(i),
+			AnUInt32:    uint32(i),
+			AnUInt64:    uint64(i),
+			AFloat32:    1 / float32(i),
+			AFloat64:    1 / float64(i),
+			AComplex64:  140,
+			AComplex128: 150,
+			APointer:    &i,
+			AnArray:     [6]uint8{uint8(i), uint8(i), uint8(i), uint8(i), uint8(i), uint8(i)},
+			ASlice:      []uint8{uint8(i), uint8(i), uint8(i), uint8(i), uint8(i), uint8(i), uint8(i), uint8(i), uint8(i)},
+			AString:     "Run #" + strconv.Itoa(i),
+		}
+		mutate, err := NewPutStrRef(context.Background(), "", "", str)
+		if err != nil {
+			b.Errorf("Error creating mutate: %v", err)
+		}
+		mutate.SetRegion(&regioninfo.Info{})
+		mutate.Serialize()
+	}
 }
