aa7efdc3011e33707ddaa816d2d2c3a8565b7263
Benoit Sigoure <tsunanet@gmail.com>
2016-09-19 11:17:33 -0700

    test/mock: Re-generate mock file, add test and use `go generate`.
    The mock had gone stale after `Close()` was added in 62ec0d89,
    so the test now prevents the code from compiling if the mock
    doesn't implement the interface.  Using `go generate` makes it
    easier to re-generate the mock code.
    
    Change-Id: I610da24134ba94ab977a6eee775cdd761409f166

---
+++ test/mock/gen.go
@@ -0,0 +1,8 @@
+// Copyright (C) 2016  The GoHBase Authors.  All rights reserved.
+// This file is part of GoHBase.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the COPYING file.
+
+package mock
+
+//go:generate mockgen -package=mock -source=../../client.go -destination=client.go
