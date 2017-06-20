48be9382938282a762677cdac6cf4d86c419faba
Ben Guidarelli <ben.guidarelli@twcable.com>
2017-06-20 15:28:44 -0400

    again

--- integration_test.go
+++ integration_test.go
@@ -119,7 +119,7 @@ func TestMain(m *testing.M) {
 //Test retrieval of cluster status
 func TestClusterStatus(t *testing.T) {
 	ac := gohbase.NewAdminClient(*host)
-	defer ac.(Client).Close()
+	defer ac.(gohbase.Client).Close()
 
 	stats, err := ac.ClusterStatus()
 	if err != nil {
