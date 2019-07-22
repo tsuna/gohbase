3dd1fa8c4feb27a069eaf9a64a2e8a9ed5538e43
Aaron Delaney <apd@arista.com>
2019-07-22 18:57:02 +0100

    hrpc, gohbase: Add snapshot creation and deletion
    This commit introduces the ability to add and delete snapshots via the
    admin client.
    
    Due to the snapshot API being slightly different to other admin calls,
    we introduce a new option SnapshotValidateInterval which allows us to
    know when the Snapshot has been created.
    
    More features will be introduced in the future to ensure snapshots that
    are created are valid.

---
+++ hrpc/snapshot.go
@@ -0,0 +1,167 @@
+// Copyright (C) 2019  The GoHBase Authors.  All rights reserved.
+// This file is part of GoHBase.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the COPYING file.
+
+package hrpc
+
+import (
+	"context"
+	"errors"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/tsuna/gohbase/pb"
+)
+
+type snap struct {
+	name         string
+	table        string
+	snapshotType *pb.SnapshotDescription_Type
+	version      int32
+	owner        string
+}
+
+func (s *snap) ToProto() *pb.SnapshotDescription {
+	return &pb.SnapshotDescription{
+		Type:    s.snapshotType,
+		Table:   proto.String(s.table),
+		Name:    proto.String(s.name),
+		Version: proto.Int32(s.version),
+		Owner:   proto.String(s.owner),
+	}
+}
+
+func (s *snap) Version(v int32) {
+	s.version = v
+}
+
+func (s *snap) Owner(o string) {
+	s.owner = o
+}
+
+func (s *snap) Type(t pb.SnapshotDescription_Type) {
+	s.snapshotType = &t
+}
+
+type snapshotSettable interface {
+	Version(int32)
+	Owner(string)
+	Type(pb.SnapshotDescription_Type)
+}
+
+// SnapshotVersion sets the version of the snapshot.
+func SnapshotVersion(v int32) func(Call) error {
+	return func(g Call) error {
+		sn, ok := g.(snapshotSettable)
+		if !ok {
+			return errors.New("'SnapshotVersion' option can only be used with Snapshot queries")
+		}
+		sn.Version(v)
+		return nil
+	}
+}
+
+// SnapshotOwner sets the owner of the snapshot.
+func SnapshotOwner(o string) func(Call) error {
+	return func(g Call) error {
+		sn, ok := g.(snapshotSettable)
+		if !ok {
+			return errors.New("'SnapshotOwner' option can only be used with Snapshot queries")
+		}
+		sn.Owner(o)
+		return nil
+	}
+}
+
+// SnapshotSkipFlush disables hbase flushing when creating the snapshot.
+func SnapshotSkipFlush() func(Call) error {
+	return func(g Call) error {
+		sn, ok := g.(snapshotSettable)
+		if !ok {
+			return errors.New("'SnapshotSkipFlush' option can only be used with Snapshot queries")
+		}
+		sn.Type(pb.SnapshotDescription_SKIPFLUSH)
+		return nil
+	}
+}
+
+// Snapshot represents a Snapshot HBase call
+type Snapshot struct {
+	base
+	snap
+}
+
+// NewSnapshot creates a new Snapshot request that will request a
+// new snapshot in HBase.
+func NewSnapshot(ctx context.Context, name string, table string) *Snapshot {
+	return &Snapshot{
+		base{
+			table:    []byte(table),
+			ctx:      ctx,
+			resultch: make(chan RPCResult, 1),
+		},
+		snap{
+			name: name, table: table,
+		},
+	}
+}
+
+// Name returns the name of this RPC call.
+func (sr *Snapshot) Name() string {
+	return "Snapshot"
+}
+
+// ToProto converts the RPC into a protobuf message
+func (sr *Snapshot) ToProto() proto.Message {
+	return &pb.SnapshotRequest{Snapshot: sr.snap.ToProto()}
+}
+
+// NewResponse creates an empty protobuf message to read the response of this
+// RPC.
+func (sr *Snapshot) NewResponse() proto.Message {
+	return &pb.SnapshotResponse{}
+}
+
+// SnapshotDone represents an IsSnapshotDone HBase call
+type SnapshotDone struct {
+	*Snapshot
+}
+
+// NewSnapshotDone creates a new SnapshotDone request that will request a
+// new snapshot in HBase.
+func NewSnapshotDone(t *Snapshot) *SnapshotDone {
+	return &SnapshotDone{t}
+}
+
+// Name returns the name of this RPC call.
+func (sr *SnapshotDone) Name() string {
+	return "IsSnapshotDone"
+}
+
+// NewResponse creates an empty protobuf message to read the response of this
+// RPC.
+func (sr *SnapshotDone) NewResponse() proto.Message {
+	return &pb.IsSnapshotDoneResponse{}
+}
+
+// DeleteSnapshot represents a DeleteSnapshot HBase call
+type DeleteSnapshot struct {
+	*Snapshot
+}
+
+// NewDeleteSnapshot creates a new DeleteSnapshot request that will request a
+// new snapshot in HBase.
+func NewDeleteSnapshot(t *Snapshot) *DeleteSnapshot {
+	return &DeleteSnapshot{t}
+}
+
+// Name returns the name of this RPC call.
+func (sr *DeleteSnapshot) Name() string {
+	return "DeleteSnapshot"
+}
+
+// NewResponse creates an empty protobuf message to read the response of this
+// RPC.
+func (sr *DeleteSnapshot) NewResponse() proto.Message {
+	return &pb.DeleteSnapshotResponse{}
+}
