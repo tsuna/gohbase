564cc046d59a549acf3735c26a566303b4dd3f33
Claire Iacono <ciacono@arista.com>
2024-11-25 11:57:50 -0800

    test/mock: fix typo in AdminClient mock file name

--- test/mock/gen.go
+++ test/mock/gen.go
@@ -10,7 +10,7 @@ package mock
 // then run 'go generate' to auto-generate mock_client.
 
 //go:generate mockgen -destination=client.go -package=mock github.com/tsuna/gohbase Client
-//go:generate mockgen -destination=adming_client.go -package=mock github.com/tsuna/gohbase AdminClient
+//go:generate mockgen -destination=admin_client.go -package=mock github.com/tsuna/gohbase AdminClient
 //go:generate mockgen -destination=conn.go -package=mock net Conn
 //go:generate mockgen -destination=call.go -package=mock github.com/tsuna/gohbase/hrpc Call
 //go:generate mockgen -destination=zk/client.go -package=mock github.com/tsuna/gohbase/zk Client
